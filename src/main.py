9a52868f65a449626ba59f41c053e41b459e7dab
MatureKid <157224899+PeafulLeopold@users.noreply.github.com>
2025-02-20 20:07:34 +0300

    Update main.py

---
+++ src/main.py
@@ -0,0 +1,23 @@
+from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtGui import QPixmap
+from frontend.designs.design import Ui_Main
+
+from backend.get_map_image import get_map_image
+
+
+class Main(QMainWindow, Ui_Main):
+    def __init__(self):
+        super.__init__()
+        self.ready_btn.clicked.connect(self.set_map_image)
+
+    def set_map_image(self):
+        long = int(self.longitude.text())
+        latt = int(self.width.text())
+        sc = int(self.coord.text())
+
+        img = get_map_image(longitude=long, lattitude=latt, scale=sc)
+
+        if img:
+            self.map.setPixmap(QPixmap(img)) 
+        else:
+            print('Не удалось получить изображение карты...')
