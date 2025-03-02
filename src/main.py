23826345d6a32da246eb7341fb33c44985435ae7
Bogdan Myasnikov <voennyo@yandex.ru>
2025-03-02 20:32:05 +0300

    поиск объекта на карте

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
-from PyQt6.QtWidgets import QMainWindow, QApplication
-from PyQt6.QtGui import QPixmap, QKeyEvent
+import sys
+from PyQt6.QtWidgets import QMainWindow, QApplication, QLineEdit, QPushButton, QLabel
+from PyQt6.QtGui import QPixmap, QKeyEvent, QFont
 from PyQt6 import uic
 from PyQt6.QtCore import Qt
-import sys
-from get_map_image import get_map_image
+from utils import *
+from io import BytesIO
 
 
 class Main(QMainWindow):
@@ -11,13 +12,33 @@ class Main(QMainWindow):
         super().__init__(*args, **kwargs)
         uic.loadUi("./data/design.ui", self)
         self.setWindowTitle("Карта")
+
+        """
+        Обновление дизайна: поле ввода топонима для поиска
+        """
+
+        self.search_input = QLineEdit(self)
+        self.search_input.setStyleSheet("background-color: white;")
+        self.search_input.setGeometry(42, 520, 317, 35)
+
+        self.search_label = QLabel(self)
+        self.search_label.setText('Найти объект на карте:')
+        self.search_label.setStyleSheet("color: white; font-size: 20pt; font: large 'Times New Roman'")
+        self.search_label.setGeometry(42, 480, 317, 25)
+        
+        self.search_button = QPushButton(self)
+        self.search_button.setText('Найти объект на карте:')
+        self.search_button.setStyleSheet("background-color: white; color: black; font-size: 17pt; font: large 'Times New Roman'")
+        self.search_button.setGeometry(42, 570, 317, 80)
+
         self.ready_btn.clicked.connect(self.set_map_image)
         self.theme_btn.clicked.connect(self.update_theme)
+        self.search_button.clicked.connect(self.search_toponym)
         self.scale = 0
         self.longitude_value = 0
         self.lattitude_value = 0
-        self.theme = 'light'
-
+        self.theme = "light"
+        self.point_status = False   # Флаг для метки в середине изображения
 
     def set_map_image(self):
         try:
@@ -28,26 +49,28 @@ class Main(QMainWindow):
             print("Введите числа")
             return 0
 
-        img = get_map_image(self.longitude_value, self.lattitude_value, self.scale, self.theme)
+        img = get_map_image(
+            self.longitude_value, self.lattitude_value, self.scale, self.theme
+        )
 
         if img[0] == 200:
-            self.map.setPixmap(QPixmap(img[1])) 
+            self.map.setPixmap(QPixmap(img[1]))
         else:
-            print('Не удалось получить изображение карты...')
+            print("Не удалось получить изображение карты...")
             print(img)
-    
+
     def keyPressEvent(self, event: QKeyEvent):
         self.setFocus()
         if event.key() == Qt.Key.Key_PageUp:
             if self.scale < 21:
-                self.scale += 1 
+                self.scale += 1
                 self.update_map()
 
         elif event.key() == Qt.Key.Key_PageDown:
             if self.scale > 0:
                 self.scale -= 1
                 self.update_map()
-        
+
         elif event.key() == Qt.Key.Key_Up:
             if self.scale > 0:
                 self.longitude_value += 0.001
@@ -57,7 +80,7 @@ class Main(QMainWindow):
             if self.scale > 0:
                 self.longitude_value -= 0.001
                 self.update_map()
-        
+
         elif event.key() == Qt.Key.Key_Left:
             if self.scale > 0:
                 self.lattitude_value -= 0.001
@@ -67,25 +90,51 @@ class Main(QMainWindow):
             if self.scale > 0:
                 self.lattitude_value += 0.001
                 self.update_map()
-    
+        elif event.key() in (Qt.Key.Key_Return, Qt.Key.Key_Enter):
+            self.search_toponym()
+
     def update_theme(self):
-        if self.theme == 'light':
-            self.theme = 'dark'
-            self.theme_btn.setText('Светлая')
+        if self.theme == "light":
+            self.theme = "dark"
+            self.theme_btn.setText("Светлая")
         else:
-            self.theme = 'light'
-            self.theme_btn.setText('Тёмная')
+            self.theme = "light"
+            self.theme_btn.setText("Тёмная")
 
         self.update_map()
-    
+
     def update_map(self):
-        img = get_map_image(self.longitude_value, self.lattitude_value, self.scale, self.theme)
+        img = get_map_image(
+            self.longitude_value, self.lattitude_value, self.scale, self.theme
+        )
 
         if img[0] == 200:
-            self.map.setPixmap(QPixmap(img[1])) 
+            if not self.point_status:
+                self.map.setPixmap(QPixmap(img[1]))
+            else:
+                image = Image.open(img[1])
+                image = (image)
+                
+                buffer = BytesIO()
+                image.save(buffer, format="PNG")
+                buffer.seek(0)
+                pixmap = QPixmap()
+                pixmap.loadFromData(buffer.getvalue())
+                self.map.setPixmap(pixmap)
+                self.map.repaint()
         else:
-            print('Не удалось получить изображение карты...')
+            print("Не удалось получить изображение карты...")
             print(img)
+    
+    def search_toponym(self):
+        toponym = self.search_input.text()
+
+        if toponym:
+            self.longitude_value, self.lattitude_value = get_toponym_ll(toponym)
+            self.scale = 17
+            self.point_status = True
+        
+        self.update_map()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
---
+++ src/utils.py
@@ -0,0 +1,62 @@
+import requests
+from PIL import Image, ImageDraw
+
+
+map_api_server = "https://static-maps.yandex.ru/1.x/"
+map_apikey = "55515925-9197-4295-b309-a9fe09499027"
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/?"
+geocoder_apikey = "8013b162-6b42-4997-9691-77b7074026e0"
+
+
+def get_map_image(longitude, lattitude, scale, theme):
+    try:
+        map_params = {
+            # "ll": ",".join([longitude, lattitude]),
+            "ll": ",".join([str(longitude), str(lattitude)]),
+            "z": scale,
+            "theme": theme,
+            "apikey": map_apikey,
+            "size": "601,421",
+            "l": "map",
+        }
+
+        response = requests.get(map_api_server, params=map_params)    
+    except Exception as e:
+        return 500, e
+    
+    if response.status_code == 200:
+        with open("data/map.png", "wb") as file:
+            file.write(response.content)
+        return response.status_code, "data/map.png"
+    else:
+        return response.status_code, response.content
+
+
+def get_toponym_ll(geocode):
+    geocoder_params = {
+    "apikey": geocoder_apikey,
+    "geocode": geocode,
+    "format": "json"}
+
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+
+    if response:
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        toponym_coodrinates = toponym["Point"]["pos"]
+        return toponym_coodrinates.split(" ")
+
+def draw_point(image):
+    draw = ImageDraw.Draw(image)
+    width, height = image.size
+    
+    center_x = width // 2
+    center_y = height // 2
+    point_radius = 5
+    
+    draw.ellipse((center_x - point_radius, center_y - point_radius, 
+        center_x + point_radius, center_y + point_radius),
+        fill="red")
+
+    return image
+
