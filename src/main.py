33d6c3c2d659c417f917f0c7b5277643c43361cc
Gelzy788 <negm.ali@mail.ru>
2025-03-02 16:36:48 +0300

    Реализовано масштабирование на клавиши pgup и pgdn

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import QMainWindow, QApplication
-from PyQt6.QtGui import QPixmap
+from PyQt6.QtGui import QPixmap, QKeyEvent
 from PyQt6 import uic
+from PyQt6.QtCore import Qt
 import sys
 from get_map_image import get_map_image
 
@@ -11,13 +12,37 @@ class Main(QMainWindow):
         uic.loadUi("./data/design.ui", self)
         self.setWindowTitle("Карта")
         self.ready_btn.clicked.connect(self.set_map_image)
+        self.scale = 0.005  
+        self.longitude_value = 0
+        self.lattitude_value = 0
 
     def set_map_image(self):
-        long = self.longitude.text()
-        latt = self.width.text()
-        sc = int(self.coord.text())
+        self.longitude_value = self.longitude.text()
+        self.lattitude_value = self.width.text()
+        self.scale = int(self.coord.text())
 
-        img = get_map_image(longitude=long, lattitude=latt, scale=sc)
+        img = get_map_image(self.longitude_value, self.lattitude_value, self.scale)
+
+        if img[0] == 200:
+            self.map.setPixmap(QPixmap(img[1])) 
+        else:
+            print('Не удалось получить изображение карты...')
+            print(img)
+    
+    def keyPressEvent(self, event: QKeyEvent):
+        if event.key() == Qt.Key.Key_PageUp:
+            if self.scale < 21:
+                self.scale += 1 
+                self.update_map()
+
+        elif event.key() == Qt.Key.Key_PageDown:
+            if self.scale > 0:
+                self.scale -= 1
+                self.update_map()
+        
+    
+    def update_map(self):
+        img = get_map_image(self.longitude_value, self.lattitude_value, self.scale)
 
         if img[0] == 200:
             self.map.setPixmap(QPixmap(img[1])) 
